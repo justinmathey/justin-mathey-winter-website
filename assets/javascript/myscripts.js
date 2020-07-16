df294fec72f9620c11b5042e3eeda7afb6023b50
justinmathey <53715262+justinmathey@users.noreply.github.com>
2020-07-16 15:44:10 +1000

    160720
    all done

--- assets/javascript/myscripts.js
+++ assets/javascript/myscripts.js
@@ -1,15 +1,24 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("show");
 }
+
+
+/* Close the dropdown menu if the user clicks outside of it */
+
 window.onclick = function(event) {
-    if (!event.target.matches('.button')) {
-            var dropdowns = document.getElementsByClassName("dropdown-content");
-            var i;
-for (i = 0; i < dropdowns.length; i++) {
-    var openDropdown = dropdowns[i];
-    if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
+  if (!event.target.matches('.button')) {   /* (!) is logical NOT a match */
+    var dropdowns = document.getElementsByClassName("dropdown-content"); /* */
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
     }
+  }
 }
+//Referance
+//https://www.w3schools.com/howto/howto_js_dropdown.asp
